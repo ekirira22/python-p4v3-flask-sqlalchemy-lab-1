b0e5e9ba17bef21e059e74382f02aac361c1d5d4
Eric Maranga <ekirira22@gmail.com>
2024-04-04 22:58:56 +0300

    completed assessment

--- server/app.py
+++ server/app.py
@@ -20,8 +20,35 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+    if earthquake:
+        body = earthquake.to_dict()
+        status = 200
+    else:
+        body = {'message' : f'Earthquake {id} not found.'}
+        status = 404
 
+    return make_response(body, status)
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquake_by_magnitude(magnitude):
+    earthquakes = []
+    
+    earthquakes_from_db = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    earthquakes_from_db if earthquakes_from_db else []
+
+    for earthquake in earthquakes_from_db:
+        earthquakes.append(earthquake.to_dict())
+    body = {
+        'count' : len(earthquakes_from_db),
+        'quakes' : earthquakes
+    }
+    status = 200
+
+    return make_response(body, status)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
